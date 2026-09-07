9229c72590260416264b0100dea1271a08857da1
Nimira43 <n.rabone72@aol.com>
2026-09-07 21:18:56 +0100

    Vite project created. fonts brought in.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './css/styles.css'
+
+document.querySelector('#app').innerHTML = `
+<div class='temp-container'>
+  <h1 class='logo-font'>
+    The ULA Contention
+  </h1>
+  <p class='main-font'>
+    A ULA is a type of chip where the manufacturer (Ferranti, in the Spectrum's case) produced a generic, partially-configured logic device.</p>
+</div>
+`
